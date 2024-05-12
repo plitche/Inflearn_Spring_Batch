28d732c736e7dd514a00f1d13e6786c5bea91d7b
plitche <kysu728@naver.com>
2024-05-12 21:15:10 +0900

    SimpleJob - validator()

--- Project/spring-batch-2/src/main/java/io/springbatch/springbatchlecture/Job/validator/ValidatorConfiguration.java
+++ Project/spring-batch-2/src/main/java/io/springbatch/springbatchlecture/Job/validator/ValidatorConfiguration.java
@@ -1,10 +1,12 @@
-package io.springbatch.springbatchlecture.Job.simpleJob;
+package io.springbatch.springbatchlecture.Job.validator;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.batch.core.*;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.job.DefaultJobParametersValidator;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -12,23 +14,25 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
-//@Configuration
+@Configuration
 @RequiredArgsConstructor
-public class StartNextConfiguration {
+public class ValidatorConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
-//    @Bean
+    @Bean
     public Job job() {
         return this.jobBuilderFactory.get("job1")
                 .start(step1())
                 .next(step2())
                 .next(step3())
+//                .validator(new CustomJobParametersValidator())
+                .validator(new DefaultJobParametersValidator(new String[]{"name", "date"}, new String[]{"count"}))
                 .build();
     }
 
-//    @Bean
+    @Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
                 .tasklet(new Tasklet() {
@@ -40,7 +44,7 @@ public class StartNextConfiguration {
                 }).build();
     }
 
-//    @Bean
+    @Bean
     public Step step2() {
         return stepBuilderFactory.get("step2")
                 .tasklet(new Tasklet() {
@@ -52,7 +56,7 @@ public class StartNextConfiguration {
                 }).build();
     }
 
-//    @Bean
+    @Bean
     public Step step3() {
         return stepBuilderFactory.get("step3")
                 .tasklet(new Tasklet() {
