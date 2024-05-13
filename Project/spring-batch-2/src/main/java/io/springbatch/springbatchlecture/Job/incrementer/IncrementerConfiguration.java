90d74c428d250064d56413c807309f581839ecc8
plitche <kysu728@naver.com>
2024-05-13 20:06:47 +0900

    SimpleJob - incrementer()

--- Project/spring-batch-2/src/main/java/io/springbatch/springbatchlecture/Job/incrementer/IncrementerConfiguration.java
+++ Project/spring-batch-2/src/main/java/io/springbatch/springbatchlecture/Job/incrementer/IncrementerConfiguration.java
@@ -1,4 +1,4 @@
-package io.springbatch.springbatchlecture.Job.preventRestart;
+package io.springbatch.springbatchlecture.Job.incrementer;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
@@ -6,7 +6,7 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.job.DefaultJobParametersValidator;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -14,23 +14,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
-// @Configuration
+@Configuration
 @RequiredArgsConstructor
-public class ValidatorConfiguration {
+public class IncrementerConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
-    // @Bean
+    @Bean
     public Job job() {
         return this.jobBuilderFactory.get("job1")
                 .start(step1())
                 .next(step2())
-                .preventRestart() // job이 실패하더라도 재시작 하면 안되는 경우 사용
+//                .incrementer(new CustomJobParametersIncrementer())
+                .incrementer(new RunIdIncrementer())
                 .build();
     }
 
-    // @Bean
+    @Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
                 .tasklet(new Tasklet() {
@@ -42,16 +43,14 @@ public class ValidatorConfiguration {
                 }).build();
     }
 
-    // @Bean
+    @Bean
     public Step step2() {
         return stepBuilderFactory.get("step2")
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        throw new RuntimeException("step2 was filed");
-
-                        // System.out.println("step2 was executed");
-                        // return RepeatStatus.FINISHED;
+                         System.out.println("step2 was executed");
+                         return RepeatStatus.FINISHED;
                     }
                 }).build();
     }
