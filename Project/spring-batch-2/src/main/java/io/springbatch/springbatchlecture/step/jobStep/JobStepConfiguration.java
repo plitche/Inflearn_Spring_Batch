db76f5d9e3d326f87c0c3ac8abf4b1be6cec88dd
plitche <kysu728@naver.com>
2024-05-19 18:49:02 +0900

    JobStep

--- Project/spring-batch-2/src/main/java/io/springbatch/springbatchlecture/step/jobStep/JobStepConfiguration.java
+++ Project/spring-batch-2/src/main/java/io/springbatch/springbatchlecture/step/jobStep/JobStepConfiguration.java
@@ -1,30 +1,42 @@
-package io.springbatch.springbatchlecture.step.TaskletStep4;
+package io.springbatch.springbatchlecture.step.jobStep;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.*;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.step.job.DefaultJobParametersExtractor;
+import org.springframework.batch.core.step.job.JobParametersExtractor;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
-// @Configuration
+@Configuration
 @RequiredArgsConstructor
-public class TaskletStepArchitectureConfiguration {
+public class JobStepConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
-    // @Bean
-    public Job batchJob() {
-        return this.jobBuilderFactory.get("batchJob")
-                .start(step1())
+    @Bean
+    public Job parentJob() {
+        return this.jobBuilderFactory.get("parentJob")
+                .start(jobStep(null))
                 .next(step2())
+                .build();
+    }
+
+    @Bean
+    public Step jobStep(JobLauncher jobLauncher) {
+        return this.stepBuilderFactory.get("jobStep")
+                .job(childJob())
+                .launcher(jobLauncher)
+                .parametersExtractor(jobParametersExtractor())
                 .listener(new StepExecutionListener() {
                     @Override
                     public void beforeStep(StepExecution stepExecution) {
-
+                        stepExecution.getExecutionContext().putString("name", "user1");
                     }
 
                     @Override
@@ -35,23 +47,36 @@ public class TaskletStepArchitectureConfiguration {
                 .build();
     }
 
-    // @Bean
+    private DefaultJobParametersExtractor jobParametersExtractor() {
+        DefaultJobParametersExtractor extractor = new DefaultJobParametersExtractor();
+        extractor.setKeys(new String[]{"name"});
+        return extractor;
+    }
+
+    @Bean
+    public Job childJob() {
+        return this.jobBuilderFactory.get("childJob")
+                .start(step1())
+                .build();
+    }
+
+    @Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
                 .tasklet(((stepContribution, chunkContext) -> {
-                    System.out.println("stepContribution = " + stepContribution + ", chunkContext = " + chunkContext);
+                    throw new RuntimeException("step1 was failed");
+                    // return RepeatStatus.FINISHED;
+                }))
+                .build();
+    }
+
+    @Bean
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+                .tasklet(((stepContribution, chunkContext) -> {
                     return RepeatStatus.FINISHED;
                 }))
                 .build();
     }
 
-    // @Bean
-    public Step step2() {
-        return stepBuilderFactory.get("step2")
-                .tasklet(((stepContribution, chunkContext) -> {
-                    System.out.println("stepContribution = " + stepContribution + ", chunkContext = " + chunkContext);
-                    return RepeatStatus.FINISHED;
-                }))
-                .build();
-    }
 }
